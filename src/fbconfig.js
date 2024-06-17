238e0d7f9446c6ae1185df7f174e8c72310f5e30
Alijah Taylor <alijahtaylor466@gmail.com>
2024-06-17 17:21:01 +1200

    Updated

--- src/fbconfig.js
+++ src/fbconfig.js
@@ -2,14 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
-// Your web app's Firebase configuration
+
+// Web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDU8Uayep49pHW81YTOdha9ZS-fi9pZNqU",
-    authDomain: "scp-db-10c71.firebaseapp.com",
-    projectId: "scp-db-10c71",
-    storageBucket: "scp-db-10c71.appspot.com",
-    messagingSenderId: "651252094657",
-    appId: "1:651252094657:web:ea6b1d01ac4d9052a26640"
+    apiKey: "AIzaSyAsvRg6qV88kU7ifgnYCp2qNCfSfbSjDls",
+    authDomain: "scpdb1.firebaseapp.com",
+    projectId: "scpdb1",
+    storageBucket: "scpdb1.appspot.com",
+    messagingSenderId: "869072237921",
+    appId: "1:869072237921:web:70bea451e29b11411a8120"
 };
 
 // Initialize Firebase
